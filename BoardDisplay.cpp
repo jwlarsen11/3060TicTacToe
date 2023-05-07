3734d9fb37a05e9c3cea0fb8a24a7ada9fc56971
jwlarsen11 <jacobwlarsenis@gmail.com>
2023-05-07 16:25:53 -0600

    Revert "Merge branch 'main' of https://github.com/jwlarsen11/3060TicTacToe"
    This reverts commit 4c4d38f7d4518f9712d02ac1708f9333636d6d38, reversing
    changes made to 4f257bbf22f5cf0d34810032fbc573eb3adbeb63.

--- BoardDisplay.cpp
+++ BoardDisplay.cpp
@@ -5,42 +5,12 @@
 //XMAX and YMAX must be a multiple of 9
 #define XMAX 72
 #define YMAX 36
-static char fixchax[] = {1,1,0,0,1,1,
-                        0,0,1,1,0,0,
-                        1,1,0,0,1,1
-                        };
-
-static char fixchao[] = {0,1,1,1,1,0,
-                        1,1,0,0,1,1,
-                        0,1,1,1,1,0
-                        };
-static char beegx[] = {1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,
-                      1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,
-                      0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,
-                      0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,
-                      0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,
-                      0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,
-                      0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,
-                      0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,
-                      1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,
-                      1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1
-                      };
-
-static char beego[] = {0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,
-                      0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,
-                      0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,
-                      1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,
-                      1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,
-                      1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,
-                      1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,
-                      0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,
-                      0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,
-                      0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0
-                      };
 
 typedef struct {
     int x;
     int y;
+    int fichasant[2];           // Check if the chip is in the same place.
+    int fichasjuego[2];         // Number of chips in game for each player.
     char fichas[3][3];          // Chips of the gameboard. (0 -> Empty / 1 -> X / 2 -> O)
     int jugador;                // Actual player.
     int movimientos;            // Number of moves.
@@ -99,68 +69,6 @@ void drawBoard(Gameboard *board, int color, int grid)      // Draw gameboard
   attroff(COLOR_PAIR(color));
 }//drawBoard
 
-//Drawing the chips
-void drawChips(int *dest, char chip, Gameboard *board)
-{
-    int i, j;
-    int y, x;
-    
-    for (i=0; i<3; i++)     // Chip height
-        for (j=0; j<6; j++)     // Chip width
-        {
-            y = YMAX/9 * dest[0] + i + board->y + 1;
-            x = XMAX/9 * dest[1] + j + board->x + 2;
-            if (fixchax[j+6*i] == 1 && chip == 'x')          // If X chip...
-            {
-                attron(COLOR_PAIR(4));
-                mvaddch(y, x, '#');
-                attroff(COLOR_PAIR(4));
-            }
-            
-            else if (fixchao[j+6*i] == 1 && chip == 'o')     // If O chip...
-            {
-                attron(COLOR_PAIR(3));
-                mvaddch(y, x, '#');
-                attroff(COLOR_PAIR(3));
-            }
-            
-            else    // Print spaces.
-            {
-                //
-            }
-        }
-}
-
-void drawBeegChips(int *dest, char chip, Gameboard *board){
-    int i, j;
-    int y, x;
-    
-    for (i=0; i<10; i++)     // Chip height
-        for (j=0; j<16; j++)     // Chip width
-        {
-            y = YMAX/3 * dest[0] + i + board->y + 2;
-            x = XMAX/3 * dest[1] + j + board->x + 5;
-            if (fixchax[j+16*i] == 1 && chip == 'x')          // If X chip...
-            {
-                attron(COLOR_PAIR(4));
-                mvaddch(y, x, '#');
-                attroff(COLOR_PAIR(4));
-            }
-            
-            else if (fixchao[j+16*i] == 1 && chip == 'o')     // If O chip...
-            {
-                attron(COLOR_PAIR(3));
-                mvaddch(y, x, '#');
-                attroff(COLOR_PAIR(3));
-            }
-            
-            else    // Print spaces.
-            {
-                //
-            }
-        }
-}//drawBeegChips
- 
 int main()
 {
     Gameboard Board;
@@ -168,21 +76,17 @@ int main()
     // Centering the gameboard on the screen.
     Board.x = (80-XMAX)/2;
     Board.y = 3;
-    int dest[9];
-  
+
     initscr();
     start_color();
     noecho();
     cbreak;
     curs_set(FALSE);        // Disable the console cursor.
-    init_pair(1, COLOR_CYAN, COLOR_BLACK); //3x3 color
-    init_pair(2, COLOR_WHITE,COLOR_BLACK); // 9x9 color
-    init_pair(3, COLOR_YELLOW,COLOR_BLACK);// o color
-    init_pair(4, COLOR_GREEN,COLOR_BLACK);//x color
+    init_pair(1, COLOR_CYAN, COLOR_BLACK);
+    init_pair(2, COLOR_WHITE,COLOR_BLACK);
 
     drawBoard(&Board,2,9);
     drawBoard(&Board,1,3);
-    drawChips(dest, 'x', &Board);
     
     getch();
     resetty();
